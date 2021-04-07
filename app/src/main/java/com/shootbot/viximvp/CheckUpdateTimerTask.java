ec468f805a4fa0d94227816162605181e31970bd
Timur Nurmagambetov <timporin@gmail.com>
2021-04-07 16:38:46 +0600

    without pushy 1

---
+++ app/src/main/java/com/shootbot/viximvp/CheckUpdateTimerTask.java
@@ -0,0 +1,15 @@
+package com.shootbot.viximvp;
+
+import android.os.Handler;
+
+import java.util.TimerTask;
+
+public class CheckUpdateTimerTask extends TimerTask {
+    public CheckUpdateTimerTask(Handler handler) {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ app/src/main/java/com/shootbot/viximvp/PushService.java
@@ -0,0 +1,56 @@
+package com.shootbot.viximvp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.Timer;
+
+
+public class PushService extends Service {
+    public PushService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    static boolean isStarted = false;
+    static Timer timer;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            if (!isStarted) {
+                timer = new Timer();
+                timer.schedule(new CheckUpdateTimerTask(new Handler()), 60000L, 5L * 60L * 1000);
+                isStarted = true;
+            }
+        } catch (Exception e) {
+            Log.d("PushService", e.getMessage());
+            isStarted = false;
+        }
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public boolean stopService(Intent name) {
+        timer.cancel();
+        isStarted = false;
+        timer = null;
+        return super.stopService(name);
+    }
+
+
+    public static void start(Context cx) {
+        if (!isStarted) {
+            cx.startService(new Intent(cx, PushService.class));
+        }
+    }
+
+}
